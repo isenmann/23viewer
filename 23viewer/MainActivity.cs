8f440bf0aff5d3f452e94b51faefeaa105b7456e
isenmann <daniel@archlinux.org>
2016-02-22 22:17:27 +0100

    First commit of 23 viewer This is just the splash screen and first activity with nothing on it

---
+++ 23viewer/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace viewer
+{
+	[Activity (Label = "23 viewer", Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			//Button button = FindViewById<Button> (Resource.Id.myButton);
+		}
+	}
+}
---
+++ 23viewer/SplashActivity.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+using System.Threading.Tasks;
+
+namespace viewer
+{
+	[Activity(Theme = "@style/twentyThreeviewer.Splash", MainLauncher = true, NoHistory = true)]
+	public class SplashActivity : AppCompatActivity
+	{
+		public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+		{
+			base.OnCreate(savedInstanceState, persistentState);
+
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+
+			Task startupWork = new Task(() => {
+				Task.Delay(20000);  // Simulate a bit of startup work.
+			});
+
+			startupWork.ContinueWith(t => {
+				StartActivity(new Intent(Application.Context, typeof(MainActivity)));	
+			}, TaskScheduler.FromCurrentSynchronizationContext());
+
+			startupWork.Start();
+		}
+	}
+}
+
